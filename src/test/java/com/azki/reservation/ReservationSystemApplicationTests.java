a6f7be081b7342c5d3a2417292f4a5b84863b350
i.sadafi <i.sadafi@doctoreto.com>
2026-07-16 19:49:16 +0330

    chore: initialize project repository

---
+++ src/test/java/com/azki/reservation/ReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.azki.reservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
